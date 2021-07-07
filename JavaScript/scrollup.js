139507856947ef861885265f3e6eb1a1d72fde54
back2021 <81551903+back2021@users.noreply.github.com>
2021-07-08 08:38:05 +0900

    Update scrollup.js

--- JavaScript/scrollup.js
+++ JavaScript/scrollup.js
@@ -1,18 +1,4 @@
 $(function(){
-	$("#menudiv").on("mouseenter",function(){
-			$("#downmenu > ul").css("display","inline-block")
-			$(".sangdanbar").css("background","rgba(0,0,0,0.9)")
-			$("#headerbar").stop().animate({
-				height:180
-			},1000);
-		});
-		$("#headerbar").on("mouseleave",function(){
-			$("#downmenu > ul").css("display","none")
-			$(".sangdanbar").css("background","rgba(0,0,0,0.7)")
-			$(this).stop().animate({
-				height:80
-			},1000);
-		});
 	$(window).scroll(function () {
 	     var quickHeight=$(document).scrollTop(); //스크롤 높이가 500 이상이면 나타나기
 	     if (500 <= quickHeight ) {
