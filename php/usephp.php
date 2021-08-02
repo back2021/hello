595e407276170362f53192a487d7d9b2b42448cb
back2021 <81551903+back2021@users.noreply.github.com>
2021-08-03 08:42:52 +0900

    유용한 php 함수

---
+++ php/usephp.php
@@ -0,0 +1,16 @@
+<?php
+//php 유용한 함수
+
+// strpos 문자열 포함하고 있는지 확인하는 함수
+if(strpos($target, '확인') != false){
+	//$target 안에 '확인'이라는 글자 있음
+}else{
+	//$target 안에 '확인'이라는 글자 없음
+}
+
+// php intval 문자를 정수로
+$string = '0802';
+// 문자 '0802'를
+intval($string);
+// 숫자 '802'로
+?>
