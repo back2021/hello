00eceaa82584bc9d1a8e0d9d7009f1b4a51e30ee
back2021 <81551903+back2021@users.noreply.github.com>
2021-08-04 10:42:23 +0900

    유용한 php 2

--- php/usephp.php
+++ php/usephp.php
@@ -8,6 +8,15 @@ if(strpos($target, '확인') != false){
 	//$target 안에 '확인'이라는 글자 없음
 }
 
+if($result){
+	//$result는 sql문 돌린거임
+}else{
+	echo "문제발생".mysqli_error($connect);
+	//$connect는 연결한거임
+	//mysql 에러출력한다고 생각하자
+}
+
+
 // php intval 문자를 정수로
 $string = '0802';
 // 문자 '0802'를
