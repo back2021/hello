879008414b6f7da34947fa5a3c6b8086ca15996f
back2021 <3859114@gmail.com>
2021-12-02 13:57:15 +0900

    쉿

--- php/isset.php
+++ php/isset.php
@@ -2,9 +2,9 @@
 	
 	// isset 함수를 이용해서 post 방식으로 data 가 정의되었는지 확인 가능하다
     if(isset($_POST['data'])){
-        $current = $_POST['data'];
+        $result = $_POST['data'];
     }else {
-        $current = 0;
+        $result = 0;
     }
 
 ?>
